9fe42f3662366596bfc589a4719b1f0755b96206
stta-odoo <stta@odoo.com>
2024-03-21 09:30:24 +0800

    [Tutorial] Exercise 9.1

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class Property(models.Model):
     _name = "estate.property"
@@ -39,3 +39,10 @@ class Property(models.Model):
     property_tag_ids = fields.Many2many("estate.property.tag", string="Property Tags")
 
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+    total_area = fields.Integer(compute="_compute_total_area")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
