6333f78493cf46d0d662896323fd75dea3b8c057
stta-odoo <stta@odoo.com>
2024-03-21 15:45:54 +0800

    [Tutorial] Exercise 12.4

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,7 +3,8 @@ from odoo import fields, models
 class Property_Type(models.Model):
     _name = "estate.property.type"
     _description = "Property type"
-    _order = "name"
+    _order = "sequence, name"
     name = fields.Char(required=True)
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
     property_ids = fields.One2many("estate.property", "property_type_id")
     
