997df3bb33a4bc112a04c74da187256c64ba91fd
stta-odoo <stta@odoo.com>
2024-03-21 15:28:02 +0800

    [Tutorial] Exercise 12.1

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,3 +4,5 @@ class Property_Type(models.Model):
     _name = "estate.property.type"
     _description = "Property type"
     name = fields.Char(required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id")
+    
