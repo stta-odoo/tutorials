ca1b5405fee2c8fa0fd9133515abd831e23384fc
stta-odoo <stta@odoo.com>
2024-03-22 14:46:53 +0800

    [Tutorial] Exercise 14.1

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Real Estate',
+    'application': True,
+    'depends': ['estate', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        # 'views/estate_account_views.xml',
+    ]
+}
---
+++ estate_account/models/estate_account.py
@@ -0,0 +1,5 @@
+from odoo import fields, models
+
+class Property_Tag(models.Model):
+    _name = "estate.account"
+    _description = "Estate accounting"
