19b1d2e15a34fbaae2cdb45fc629e5a83580da85
stta-odoo <stta@odoo.com>
2024-03-22 10:44:56 +0800

    [Tutorial] Exercise 12.13

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -10,6 +10,7 @@ class Property_Offer(models.Model):
                              copy=False)
     buyer_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    property_type_id = fields.Many2one(related="property_id.property_type_id", store=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
     
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class Property_Type(models.Model):
     _name = "estate.property.type"
@@ -7,4 +7,13 @@ class Property_Type(models.Model):
     name = fields.Char(required=True)
     sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
     property_ids = fields.One2many("estate.property", "property_type_id")
+    offer_ids = fields.One2many(related="property_ids.offer_ids")
+    offer_count = fields.Integer(compute="_compute_offer_count")
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = 0
+            for property in self.property_ids:
+                record.offer_count += len(property.offer_ids)
     
