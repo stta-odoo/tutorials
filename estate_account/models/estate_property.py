de89124236f38901d3c18ff7ffeef27f388dfe34
stta-odoo <stta@odoo.com>
2024-03-25 09:44:51 +0800

    [Tutorial] Exercise 14.4

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,11 +1,12 @@
 from odoo import Command, models
-from odoo.exceptions import UserError
+from odoo.tools import float_utils
 
 class Property(models.Model):
     _inherit = "estate.property"
 
     def action_sold_click(self):
         # self._create_invoice(self)
+        _precision = self.env["decimal.precision"].precision_get("Account")
         journal = self.env["account.journal"].search([
                 *self.env["account.journal"]._check_company_domain(self.env.company),
                 ("type", "=", "sale"),
@@ -15,6 +16,15 @@ class Property(models.Model):
             "move_type": "out_invoice",
             "partner_id": self.buyer_id.id,
             "journal_id": journal.id,
+            "invoice_line_ids": [Command.create({
+                "name":"Down Payment", 
+                "quantity":1,
+                "price_unit": float_utils.float_round(0.06*self.best_price, precision_digits=_precision),
+            }),Command.create({
+                "name":"Administrative Fee", 
+                "quantity":1,
+                "price_unit": 100,
+            })]
         }
         self.env["account.move"].create(vals)
 
